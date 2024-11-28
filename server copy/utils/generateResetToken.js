d3298f798c0b0ad1393a9978f5c7993ec4152b24
tranvu <teddiebrwn@teddies-MacBook-Pro.local>
2024-11-28 16:59:28 +0700

    update proxy default admin3002 user3001 server3000

--- server copy/utils/generateResetToken.js
+++
@@ -1,8 +0,0 @@
-const crypto = require("crypto");
-const generateResetToken = () => {
-  const resetToken = crypto.randomBytes(32).toString("hex");
-  const resetExpiry = new Date(Date.now() + 15 * 60 * 1000);
-  return { resetToken, resetExpiry };
-};
-
-module.exports = generateResetToken;
